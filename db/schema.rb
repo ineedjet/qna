e82695f34631bf211245530a7898d492793617c1
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-14 00:29:17 +0300

    add Answer model and validation test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813210257) do
+ActiveRecord::Schema.define(version: 20170813212730) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
