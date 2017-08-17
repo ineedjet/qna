bffe5b9b37b42fbc5e322234be98770488ffb863
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-17 03:06:32 +0300

    question GET show spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+    let(:question) {create(:question)}
+
+    before do
+      get :show, params: {id: question}
+    end
+
+    it 'assigns the quested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
