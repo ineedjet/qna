4f8cef39e5f70a7adb6c1054c941801b388d1baf
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-17 18:46:33 +0300

    question destroy spec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -113,4 +113,17 @@ RSpec.describe QuestionsController, type: :controller do
 
   end
 
+  describe  'DELETE #destroy' do
+    before { question }
+
+    it 'deletes question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect to index view' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to questions_path
+    end
+  end
+
 end
