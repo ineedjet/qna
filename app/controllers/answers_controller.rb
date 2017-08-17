b586653382942dbbd2e8ac569dcf284a7a9529cf
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-18 01:07:58 +0300

    answer POST #create spec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,28 @@
 class AnswersController < ApplicationController
-  before_action :load_question, only: [:new]
+  before_action :load_question, only: [:new, :create]
 
   def new
     @answer = Answer.new(question: @question)
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    @answer.question = @question
+
+    if @answer.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   private
 
   def load_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
