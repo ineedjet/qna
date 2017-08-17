a7b48bfa0d49f1d9b1238475035e604714620854
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-18 00:02:51 +0300

    create answer new method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+  before_action :load_question, only: [:new]
+
+  def new
+    @answer = Answer.new(question: @question)
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:question_id])
+  end
 end
