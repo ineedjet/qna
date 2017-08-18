62cdfaccb18d085b3575592029362f954c10feed
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-19 00:49:52 +0300

    use @question.answers.new(answer_params)

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -6,8 +6,7 @@ class AnswersController < ApplicationController
   end
 
   def create
-    @answer = Answer.new(answer_params)
-    @answer.question = @question
+    @answer = @question.answers.new(answer_params)
 
     if @answer.save
       redirect_to @question
