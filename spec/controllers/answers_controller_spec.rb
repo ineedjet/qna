c4dfcc34c0b1aea84527744a3bf99edbc3a43980
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-18 18:48:49 +0300

    it 'saves the new answer in the database with question association' fix

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'saves the new answer in the database' do
-        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      it 'saves the new answer in the database with question association' do
+        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
       end
 
       it 'redirect to show answers question view' do
