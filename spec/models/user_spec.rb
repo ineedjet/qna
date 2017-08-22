dc5656f1a7779b0cf2d63f85d71c8594e05990be
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-22 17:32:56 +0300

    user spec fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it { should validate_presents _of :email }
-  it { should validate_presents _of :password }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
 end
