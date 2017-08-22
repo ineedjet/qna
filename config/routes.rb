464d65b52e6935b21062e41be25afc3902b4556a
Yuriy Kirillov <yurkakirillov@gmail.com>
2017-08-22 17:33:13 +0300

    setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  devise_for :users
   resources :questions do
     resources :answers
   end
+
+  root to: "questions#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
