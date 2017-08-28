564206485491cb5ebface6deaca586cd7e05b477
Юрий Кириллов <yurkakirillov@gmail.com>
2017-08-28 05:19:54 +0300

    create question assign tu user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,7 +19,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = current_user.questions.new(question_params)
 
     if @question.save
       flash[:notice] = 'Your question successfully created'
