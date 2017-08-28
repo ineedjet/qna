d2b43f9f4e5ee046fb25b4a2ec959aa026ce2673
Юрий Кириллов <yurkakirillov@gmail.com>
2017-08-28 04:58:58 +0300

    add user have many questions feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822143015) do
+ActiveRecord::Schema.define(version: 20170828014956) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170822143015) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
