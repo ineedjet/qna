cb2d81af0eafad551d7ee42cd205afbc12c397f0
Юрий Кириллов <yurkakirillov@gmail.com>
2017-08-28 06:38:08 +0300

    user - answer release

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828014956) do
+ActiveRecord::Schema.define(version: 20170828033556) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "question_id"
+    t.integer "user_id"
   end
 
   create_table "questions", force: :cascade do |t|
