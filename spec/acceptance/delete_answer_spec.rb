ead883720b6275844c8f37f16f453ff9ace009bd
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-01 01:53:05 +0300

    answer delete do not show him in list spec

--- spec/acceptance/delete_answer_spec.rb
+++ spec/acceptance/delete_answer_spec.rb
@@ -10,24 +10,31 @@ feature 'Delete answer', %q{
   given! (:user2) { create(:user) }
   given! (:question) { create :question, user: user }
   given! (:answer) { create :answer, question: question, user: user }
-  given! (:answer2) { create :answer, question: question, user: user2 }
 
-  scenario 'User delete his answer' do
+  scenario 'User try delete his answer' do
     sign_in(user)
 
     visit questions_path
     click_on question.title
-    click_on "Delete answer #{answer.id}"
+    click_on "Delete answer"
 
     expect(page).to have_content 'Your answer successfully deleted'
+    expect(page).to have_no_content answer.body
+    expect(current_path).to eq question_path(question)
   end
 
   scenario 'User try delete a strangers answer' do
-    sign_in(user)
+    sign_in(user2)
 
     visit questions_path
     click_on question.title
 
-    expect(page).to have_no_link "Delete answer #{answer2.id}"
+    expect(page).to have_no_link "Delete answer"
+    expect(current_path).to eq question_path(question)
+  end
+
+  scenario 'non-authenticated user try delete answer' do
+    visit question_path(question)
+    expect(page).to_not have_content('delete answer')
   end
 end
