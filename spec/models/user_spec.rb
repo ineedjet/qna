30cac395523647316da56c83054bdc3ba67d6cb0
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-02 03:49:30 +0300

    add check author_of? spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe User do
   it {should have_many(:answers).dependent(:destroy)}
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  context 'check author_of?' do
+    let(:user) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:question) { create(:question, user: user) }
+
+    it 'user is author' do
+      expect(user).to be_author_of(question)
+    end
+
+    it 'user is not author' do
+      expect(user2).to_not be_author_of(question)
+    end
+  end
 end
