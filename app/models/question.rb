a9be1570bc92fc4d9f0636f7a41bef767e9acf26
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-13 22:51:05 +0300

    accepts_nested_attributes_for :attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,8 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, dependent: :destroy
+
   validates :title, :body, presence: true
+
+  accepts_nested_attributes_for :attachments
 end
