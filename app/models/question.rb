7c7956c145df5fe477a26fbd3dbebb2af0a07d38
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-14 01:39:16 +0300

    make attachment polimorfing

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
-  has_many :attachments, dependent: :destroy
+  has_many :attachments, as: :attachmentable, dependent: :destroy
 
   validates :title, :body, presence: true
 
---
+++ spec/acceptance/add_file_to_answer_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'Add file to answer', %q{
+  In order to illustrate answer
+  As an answer author
+  I want to be able to attach file
+} do
+
+  given (:user) { create(:user) }
+  given (:question) { create(:question, user: user) }
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+  end
+
+  scenario 'User add file then send answer' do
+    fill_in 'Body', with: "answer body"
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Create'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+  end
+end
