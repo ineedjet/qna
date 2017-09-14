ed08488d2e41fb29522ac6f85a6590940bd9ed3e
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-15 00:47:10 +0300

    answer can has many questions spec and feature

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'devise'
 gem 'slim-rails'
 gem 'carrierwave'
 gem 'remotipart'
+gem 'cocoon'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/acceptance/add_file_to_answer_spec.rb
+++ spec/acceptance/add_file_to_answer_spec.rb
@@ -24,4 +24,21 @@ feature 'Add file to answer', %q{
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
 
   end
+
+  scenario 'User can add many files when answering', js: true do
+    fill_in 'Body', with: 'answer body'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add file'
+    within all('.nested-fields').last do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    click_on 'Create answer'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+    end
+    expect(page).to have_content 'Your answer successfully created'
+    expect(page).to_not have_css '.nested-fields'
+  end
 end
