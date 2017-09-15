f190db2778e06fa30b2b9726287dddeeaf106e9f
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-15 03:15:55 +0300

    only current user can remove attachment acceptance spec and feature

--- spec/acceptance/delete_attachment_spec.rb
+++ spec/acceptance/delete_attachment_spec.rb
@@ -7,12 +7,13 @@ feature 'Delete answer', %q{
 } do
 
   given! (:user) { create(:user) }
+  given! (:user2) { create(:user) }
   given! (:question) { create :question, user: user }
   given! (:answer) { create :answer, question: question, user: user }
   given! (:question_attachment) { create :attachment, attachable: question }
   given! (:answer_attachment) { create :attachment, attachable: answer }
 
-  scenario 'User try delete his answers attachment', js:true do
+  scenario 'User try delete his question attachment', js:true do
     sign_in(user)
 
     visit question_path(question)
@@ -27,18 +28,48 @@ feature 'Delete answer', %q{
     expect(current_path).to eq question_path(question)
   end
 
-  #scenario 'User try delete his question attachment', js:true do
-  #  sign_in(user2)
+  scenario 'User try delete strangers question attachment', js:true do
+    sign_in(user2)
+    visit question_path(question)
+    within '.question' do
+      expect(page).to have_no_link "Delete attachment"
+    end
+  end
 
-  #  visit questions_path
-  #  click_on question.title
+  scenario 'Anonimous user try delete question attachment', js:true do
+    visit question_path(question)
+    within '.question' do
+      expect(page).to have_no_link "Delete attachment"
+    end
+  end
 
-  #  expect(page).to have_no_link "Delete answer"
-  #  expect(current_path).to eq question_path(question)
-  #end
 
-  #scenario 'non-authenticated user try delete answer' do
-  #  visit question_path(question)
-  #  expect(page).to_not have_content('Delete answer')
-  #end
+  scenario 'User try delete his answer attachment', js:true do
+    sign_in(user)
+
+    visit question_path(question)
+    within '.answers' do
+      click_on "Delete attachment"
+      expect(page).to have_no_content answer_attachment.file.file.filename
+    end
+
+    expect(page).to have_content 'Your attachment successfully deleted'
+    expect(page).to have_content answer.body
+    expect(current_path).to eq question_path(question)
+  end
+
+  scenario 'User try delete strangers answer attachment', js:true do
+    sign_in(user2)
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to have_no_link "Delete attachment"
+    end
+  end
+
+  scenario 'Anonimous user try delete answer attachment', js:true do
+    visit question_path(question)
+    within '.answers' do
+      expect(page).to have_no_link "Delete attachment"
+    end
+  end
 end
