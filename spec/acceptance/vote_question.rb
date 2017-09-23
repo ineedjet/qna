ec8db04bf9f3c2563b251c3bbb0ab9746023c3c5
Юрий Кириллов <yurkakirillov@gmail.com>
2017-09-24 02:01:37 +0300

    vote def render rules and specs

--- spec/acceptance/vote_question.rb
+++ spec/acceptance/vote_question.rb
@@ -9,11 +9,15 @@ feature 'Vote question', %q{
   given! (:user) { create(:user) }
   given! (:user2) { create(:user) }
   given! (:question) { create(:question, user: user) }
+  given! (:user3) { create(:user) }
+  given! (:vote) { create(:vote, votable: question, user: user3, vote_type: 'positive') }
+
 
   scenario 'Unauthenticated  user cant vote' do
     visit question_path(question)
     expect(page).to_not have_content "vote +1"
     expect(page).to_not have_content "vote -1"
+    expect(page).to have_content "raiting: 1"
     expect(page).to_not have_content "delete vote"
   end
 
@@ -22,6 +26,7 @@ feature 'Vote question', %q{
     visit question_path(question)
     expect(page).to_not have_content "vote +1"
     expect(page).to_not have_content "vote -1"
+    expect(page).to have_content "raiting: 1"
     expect(page).to_not have_content "delete vote"
   end
 
@@ -30,6 +35,41 @@ feature 'Vote question', %q{
     visit question_path(question)
     expect(page).to have_content "vote +1"
     expect(page).to have_content "vote -1"
+    expect(page).to have_content "raiting: 1"
+    expect(page).to_not have_content "delete vote"
+  end
+
+  scenario 'Authenticated user can vote positive for strangers question' do
+    sign_in(user2)
+    visit question_path(question)
+    expect(page).to have_content "raiting: 0"
+    click_on "vote +1"
+    expect(page).to_not have_content "vote +1"
+    expect(page).to_not have_content "vote -1"
+    expect(page).to have_content "raiting: 1"
+    expect(page).to have_content "delete vote"
+  end
+
+  scenario 'Authenticated user can vote negative for strangers question' do
+    sign_in(user2)
+    visit question_path(question)
+    expect(page).to have_content "raiting: 1"
+    click_on "vote -1"
+    expect(page).to_not have_content "vote +1"
+    expect(page).to_not have_content "vote -1"
+    expect(page).to have_content "raiting: 0"
+    expect(page).to have_content "delete vote"
+  end
+
+  scenario 'Authenticated user can revote for strangers question' do
+    sign_in(user3)
+    visit question_path(question)
+    expect(page).to have_content "raiting: 1"
+    click_on "delete vote"
+    expect(page).to have_content "raiting: 0"
+    expect(page).to_not have_content "delete vote"
+    click_on "vote -1"
+    expect(page).to have_content "raiting: -1"
     expect(page).to have_content "delete vote"
   end
 end
