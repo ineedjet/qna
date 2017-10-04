34b42ed6f1df3064b06712a0c1a0c808a17de252
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-04 20:50:54 +0300

    authorization model, user has not autorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,11 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    authorization.user if authorization
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
+    user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,5 +31,32 @@ RSpec.describe User do
         expect(User.find_for_oauth(auth)).to eq user
       end
     end
+
+    context 'user has not autorization' do
+      context 'user exist' do
+        let(:auth){ OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
+
+        it 'does not create new user' do
+          expect{ User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+
+        it 'create autorization for user' do
+          expect{ User.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+        end
+
+        it 'create autorization with provider and id' do
+          user = User.find_for_oauth(auth)
+          authorization = user.authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+
+        it 'return user' do
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+
+      end
+    end
   end
 end
