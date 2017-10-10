abedd7bebec3fa63f8ea724ded0c7a107236a36b
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-10 19:11:40 +0300

    user can update and destroy Question, Answer, Comment, Attachment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,31 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
+    @user = user
+
     if user
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-        can :create, [Question, Answer, Comment, Attachment]
-      end
+      user.admin? ? admin_abilities : user_abilities
     else
-      can :read, :all
+      guest_abilities
     end
+  end
+
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def user_abilities
+    guest_abilities
+    can :create, [Question, Answer, Comment, Attachment]
+    can [:update, :destroy], [Question, Answer, Comment], user: user
+    can :destroy, Attachment, attachable: { user: user }
+  end
+
+  def admin_abilities
+    can :manage, :all
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -22,13 +22,54 @@ RSpec.describe Ability , type: :model do
 
   describe 'for user' do
     let(:user) { create :user }
+    let(:user2 ) { create :user }
+    let(:question ) { create :question, user: user }
 
-    it { should_not be_able_to :manage, :all  }
-    it { should be_able_to :read, :all  }
+    it { should_not be_able_to :manage, :all }
+    it { should be_able_to :read, :all }
 
-    it { should be_able_to :create, Question  }
-    it { should be_able_to :create, Answer  }
-    it { should be_able_to :create, Comment  }
-    it { should be_able_to :create, Attachment   }
+    context 'Question' do
+      it { should be_able_to :create, Question }
+
+      it { should be_able_to :update, create(:question, user: user), user: user }
+      it { should_not be_able_to :update, create(:question, user: user2), user: user }
+
+      it { should be_able_to :destroy, question, user: user  }
+      it { should_not be_able_to :destroy, create(:question, user: user2), user: user  }
+    end
+
+    context 'Answer' do
+      it { should be_able_to :create, Answer }
+      it { should be_able_to :update, create(:answer, question: question, user: user), user: user }
+      it { should_not be_able_to :update, create(:answer, question: question , user: user2), user: user }
+
+      it { should be_able_to :destroy, create(:answer, question: question, user: user), user: user }
+      it { should_not be_able_to :destroy, create(:answer, question: question , user: user2), user: user }
+    end
+
+    context 'Comment' do
+      it { should be_able_to :create, Comment, commentable: question }
+      it { should be_able_to :update, create(:comment, commentable: question, user: user), user: user }
+      it { should_not be_able_to :update, create(:comment, commentable: question , user: user2), user: user }
+
+      it { should be_able_to :destroy, create(:comment, commentable: question, user: user), user: user }
+      it { should_not be_able_to :destroy, create(:comment, commentable: question , user: user2), user: user }
+    end
+
+    context 'Attachment' do
+      let(:question2) { create :question, user: user2 }
+
+      it { should be_able_to :create, Attachment }
+
+      it { should be_able_to :destroy, create(:attachment, attachable: question), user: user }
+      it { should_not be_able_to :destroy, create(:attachment, attachable: question2), user: user }
+    end
+
+
+
+
+
+
+
   end
 end
