780a85dfdc00af41e99de8ec8ce7e024417cf3b8
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-12 02:01:30 +0300

    me spec and tests

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -13,5 +13,29 @@ describe 'Profile API' do
         expect(response.status).to eq 401
       end
     end
+
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let(:access_token) {create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles/me', params: { format: :json, access_token: access_token.token } }
+
+      it 'should return status 200' do
+        expect(response).to be_success
+      end
+
+      %w(email id).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr)
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contains #{attr}" do
+          expect(response.body).to_not have_json_path(attr)
+        end
+      end
+
+    end
   end
 end
