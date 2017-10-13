ab2d5bbe2de674d6fc83a55a28a35d919a20a165
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-14 01:32:31 +0300

    questions index in api

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -16,11 +16,23 @@ describe 'Profile API' do
 
     context 'authorized' do
       let(:access_token) {create(:access_token) }
+      let!(:questions) { create_list(:question, 3) }
 
       before { get '/api/v1/questions', params: { format: :json, access_token: access_token.token } }
 
       it 'should return status 200' do
         expect(response).to be_success
+      end
+
+      it 'return list of questions' do
+        expect(response.body).to have_json_size(3)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          question = questions.first
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path(attr).at_path("0/#{attr}")
+        end
       end
 
     end
