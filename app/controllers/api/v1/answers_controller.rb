8a7538763d9406bb9e4dd29d584b72af1eeceb6a
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-15 19:04:49 +0300

    create answer feature ans spec

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -8,4 +8,13 @@ class Api::V1::AnswersController < Api::V1::BaseController
     respond_with Answer.find(params[:id])
   end
 
+  def create
+    respond_with @answer = Answer.create(answer_params.merge(question_id: params[:question_id]).merge(user_id: current_resource_owner.id))
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -17,7 +17,8 @@ describe 'Profile API' do
     end
 
     context 'authorized' do
-      let(:access_token) {create(:access_token) }
+      let(:user) { create(:user) }
+      let(:access_token) {create(:access_token, resource_owner_id: user.id) }
 
       context '/index' do
         let!(:answers) { create_list(:answer, 3, question: question) }
@@ -80,6 +81,48 @@ describe 'Profile API' do
 
         it "answer contains attachments url" do
           expect(response.body).to be_json_eql(answer_attachment.file.url.to_json).at_path("attachments/0/url")
+        end
+
+      end
+
+
+      context '/create' do
+
+        context 'with valid attributes' do
+
+          it 'saves the new answer in the database' do
+            expect{
+              post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: { body: 'Answer body' } }
+            }.to change(Answer, :count).by(1)
+          end
+
+          it 'return the new answer' do
+            post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: { body: 'Answer body' } }
+            expect(response.body).to be_json_eql('Answer body'.to_json).at_path('body')
+          end
+
+          it 'check user is author' do
+            post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: { body: 'Answer body' } }
+            expect(Answer.last.user).to eq user
+          end
+
+          it 'check question association' do
+            post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: { body: 'Answer body' } }
+            expect(Answer.last.question).to eq question
+          end
+        end
+
+        context 'with invalid attributes' do
+          it 'does not saves the answer in database' do
+            expect{
+              post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: { body: '' } }
+            }.to_not change(Answer, :count)
+          end
+
+          it 'returns status 422' do
+            post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: access_token.token, answer: { body: '' } }
+            expect(response.status).to eq 422
+          end
         end
 
       end
