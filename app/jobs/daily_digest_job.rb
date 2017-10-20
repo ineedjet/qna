7e6dc97ac6ce2507c81b45af4593a7e586f737be
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-21 01:05:39 +0300

    change find_each to all

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -2,7 +2,7 @@ class DailyDigestJob < ApplicationJob
   queue_as :default
 
   def perform
-    User.find_each.each do |user|
+    User.all.each do |user|
       DailyDigestMailer.digest(user).deliver_later
     end
   end
