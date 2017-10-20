c61034518975ad2aafea4c16c6ce19b373f25ff3
Юрий Кириллов <yurkakirillov@gmail.com>
2017-10-21 01:15:48 +0300

    find_each return and fix

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -2,7 +2,7 @@ class DailyDigestJob < ApplicationJob
   queue_as :default
 
   def perform
-    User.all.each do |user|
+    User.find_each do |user|
       DailyDigestMailer.digest(user).deliver_later
     end
   end
